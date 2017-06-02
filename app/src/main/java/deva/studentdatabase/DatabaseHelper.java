52522a73af8eb837e80b5f9fb7923e90cb20dce7
devashishkatoriya <devashishkatoriya@gmail.com>
2017-06-02 17:15:38 +0530

    Added Insert() and Delete()

--- app/src/main/java/deva/studentdatabase/DatabaseHelper.java
+++ app/src/main/java/deva/studentdatabase/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package deva.studentdatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 /*
  * Created by Devashish Katoriya on 01-06-2017.
@@ -16,19 +18,53 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COL2 = "Name";
     private static final String COL3 = "Marks";
 
+    private SQLiteDatabase database;
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-
-        SQLiteDatabase database = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table "+TABLE_NAME+" (Roll INTEGER PRIMARY KEY AUTOINCREMENT,Name TEXT, Marks INTEGER)");
+        db.execSQL("create table " + TABLE_NAME + " ( "
+                + COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COL2 + " TEXT,"
+                + COL3 + " INTEGER"
+                + " ) ");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insert(int Roll,String Name,int Marks)
+    {
+        database = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COL1,Roll);
+        cv.put(COL2,Name);
+        cv.put(COL3,Marks);
+        long result = database.insert(TABLE_NAME,null,cv);
+
+        /* ALTERNATE WAY
+        database.execSQL("INSERT INTO " + TABLE_NAME + " VALUES(" + Roll + ", '" + Name + "' ," + Marks + ")");
+        */
+
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public int delete(String Name)
+    {
+        String whereClause = COL2 + "=?";
+        String []whereArgs = new String[]{Name};
+        database = this.getWritableDatabase();
+
+        int rowsDeleted = database.delete(TABLE_NAME,whereClause,whereArgs);
+        return rowsDeleted;
     }
 }
--- app/src/main/java/deva/studentdatabase/MainActivity.java
+++ app/src/main/java/deva/studentdatabase/MainActivity.java
@@ -1,11 +1,18 @@
 package deva.studentdatabase;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseHelper databaseHelper;
+    private EditText editName,editRoll,editMarks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         databaseHelper = new DatabaseHelper(this);
+
+        editMarks = (EditText) findViewById(R.id.editText_marks);
+        editName = (EditText) findViewById(R.id.editText_name);
+        editRoll = (EditText) findViewById(R.id.editText_roll);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Button exit = (Button) findViewById(R.id.button);
+        Button add = (Button) findViewById(R.id.button2);
+        Button btnDelete = (Button) findViewById(R.id.button3);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+                a.setMessage("Are you sure you want to exit ?")
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, R.string.thank_you, Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = a.create();
+                alert.setTitle("Alert!");
+                alert.show();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result = databaseHelper.insert(Integer.parseInt(editRoll.getText().toString()),
+                        editName.getText().toString(),
+                        Integer.parseInt(editMarks.getText().toString()));
+                if(result==true)
+                    Toast.makeText(MainActivity.this, "Entry Successfully added!", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Oops! Entry was not added.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int rowsDeleted = databaseHelper.delete(editName.getText().toString());
+                Toast.makeText(MainActivity.this, rowsDeleted+" rows deleted.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
